634a7cf3f9fad3a0697f405152d9c10926f15952
Cole Henderson <cwh0015@mix.wvu.edu>
2022-01-26 22:45:59 -0500

    Updating file organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopNavBarComponent } from './navigation/topnavbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
