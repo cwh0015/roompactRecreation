9e26494b4244f0b0488d1442463b098ec6f9b4a9
Cole Henderson <cwh0015@mix.wvu.edu>
2022-02-03 04:19:17 -0500

    Created management card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManagementCardComponent } from './body/managementCard/managementCard.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
@@ -10,7 +11,8 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ManagementCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/managementCard/managementCard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-managementCard',
+    templateUrl: 'managementCard.component.html',
+    styleUrls: ['managementCard.component.css']
+})
+
+export class ManagementCardComponent {
+
+}
