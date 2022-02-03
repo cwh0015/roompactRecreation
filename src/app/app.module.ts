60349f9ced1bf2177f9d96b7872a7e65b81c0fb2
Cole Henderson <cwh0015@mix.wvu.edu>
2022-02-03 05:03:36 -0500

    Created main carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainCarouselComponent } from './body/mainCarousel/mainCarousel.component';
 import { ManagementCardComponent } from './body/managementCard/managementCard.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,8 @@ import { HeaderComponent } from './header/header.component';
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    ManagementCardComponent
+    ManagementCardComponent,
+    MainCarouselComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/mainCarousel/mainCarousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-mainCarousel',
+    templateUrl: 'mainCarousel.component.html',
+    styleUrls: ['mainCarousel.component.css']
+})
+
+export class MainCarouselComponent {
+
+}
